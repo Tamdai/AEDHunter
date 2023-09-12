d19b93971fcf4227ee6fdce8f45c2df087ecf39a
toonztudio <toonztudio@gmail.com>
2023-09-12 16:56:50 +0700

    remove unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./Homepage";
-import Resultpage from "./Resultpage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
-      <Route path="/result" element={<Resultpage />} />
     </Routes>
   );
 }
